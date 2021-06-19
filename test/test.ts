b2f1470d2ae14310da0d087984b17d21068cdda4
Alexander Alemayhu <alexander@alemayhu.com>
2021-06-19 21:43:17 +0200

    test: move the loading of the artifact to setup
    We don't need to duplicate it. It's going to be the same time.

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,19 @@
 import path from "path";
 import fs from "fs";
 
-import test from "ava";
+import anyTest, { TestInterface } from "ava";
 
 import { ZipHandler } from "../src/ZipHandler";
+
+const test = anyTest as TestInterface<{ zip: ZipHandler }>;
+
+test.before(async (t) => {
+  const filePath = path.join(__dirname, "artifacts/HTML TEST.apkg");
+  const data = fs.readFileSync(filePath);
+  const zip = new ZipHandler();
+  await zip.build(data);
+  t.context = { zip: zip };
+});
 
 test("detected files", async (t) => {
   // APKG is essentially just a ZIP file
@@ -17,11 +27,6 @@ test("detected files", async (t) => {
     "collection.anki21", // This the actual db with all tables and data
     "media", // What is this??  is it related to images and scheduling???
   ];
-  // TODO: load the data in setup, should not really change
-  const filePath = path.join(__dirname, "artifacts/HTML TEST.apkg");
-  const data = fs.readFileSync(filePath);
-  const zip = new ZipHandler();
-  await zip.build(data);
-  const actual = zip.fileNames;
+  const actual = t.context.zip.fileNames;
   t.deepEqual(expected, actual);
 });
